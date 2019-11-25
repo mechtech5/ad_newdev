d9db6435e401a2fbe82211972849d64c113b4729
ritesh panchal <riteshpanchal@laxyo.in>
2019-11-25 17:48:43 +0530

    Index blade upload student changed some minor description of uploading a excel file

--- resources/views/student/upload_student/index.blade.php
+++ resources/views/student/upload_student/index.blade.php
@@ -36,9 +36,58 @@
 							<br>
 							<input type="submit" value="Submit" class="btn btn-sm btn-primary">
 							<br>
-							{{-- <h5>Before upload read terms & conditions:</h5>
-							<p>Courses Types : Post Graduat</p>
- --}}
+							<h5>Before upload read terms & conditions:</h5>
+							<ol>
+								<li>Don't change sample file header.</li> 
+								<li>Some field value format fixed. Date format is already fixed don't change</li>
+								<li>Date of birth define should not equal to current year.</li> 
+								<li>Mandatory fields: 
+									<ul>
+										<li>Qualification name</li>
+										<li>Courses name</li>
+										<li>Year of admission</li>
+										<li>Admission Batch</li>
+										<li>Semester</li>
+										<li>Admission date</li>
+										<li>Student Status</li>
+										<li>First Name</li>
+										<li>Last Name</li>
+										<li>Mobile Number</li>
+										<li>Date of Birth</li>
+										<li>Gender</li>
+										<li>Cast Category</li>
+									</ul>
+								</li>
+							</ol>
+							<p>
+								Quaification Name: 
+								<ul>
+									<li>Post Graduate (PG) -  1</li>
+									<li>Under Graduate (UG) - 2</li>
+									<li>Diploma (Diplo) - 3</li>
+								</ul>
+								Course Name:
+								<ul>
+									<li>LLM-Criminal, LLM-Civil</li>
+									<li>B.A. LL.B, BBA LL.B, B.Sc LL.B</li>
+								</ul>
+								Gender:
+								<ul>
+									<li>Male -m</li>
+									<li>Female -f</li>
+									<li>Other -t</li>
+								</ul>
+								Batch name format:
+								<ul>
+									<li>2019-2020, 2018-2019</li>									
+								</ul>
+								Student status : <span>When you define student status 'Pass' so mention 'passing date' other wise don't fill 'passing date'</span>
+								<ul>
+									<li>Running - R</li>
+									<li>Pass - P</li>
+									<li>Fail - F</li>
+								</ul> 
+							</p>
 						</div>	
 						<div class="col-md-4 " style="margin-top: 10px;">
 							<div class="row">
@@ -47,7 +96,7 @@
 										<div class="info-box " >
 											<span class="info-box-icon bg-green"><i class="fa fa-download"></i></span>
 											<div class="info-box-content">
-											<span class="info-box-text">SAMPLE DOWNLOAD</span>										
+											<span class="info-box-text">DOWNLOAD SAMPLE</span>										
 										</div>							
 										</div>
 									</div>
